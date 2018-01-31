36e98258c9c2b22af8f87e23dc451440b6b21bbb
takaaki.iida <takiida0605@gmail.com>
2018-01-31 15:54:28 +0900

    publish and subscribe bus messages

--- src/main/kotlin/org/koiki/beepro/dancer/intellij/ui/ConnectButtonListener.kt
+++ src/main/kotlin/org/koiki/beepro/dancer/intellij/ui/ConnectButtonListener.kt
@@ -1,21 +1,29 @@
 package org.koiki.beepro.dancer.intellij.ui
 
 import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.project.Project
+import org.koiki.beepro.dancer.intellij.websocket.ConnectedNotifier
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
+import javax.naming.Context
 import javax.swing.JFormattedTextField
 import javax.swing.JTextField
 
-class ConnectButtonListene : ActionListener {
-    val log = Logger.getInstance(this::class.java)
-    val textField: JTextField
+class ConnectButtonListener : ActionListener {
+    private val log = Logger.getInstance(this::class.java)
+    private val textField: JTextField
+    private val project: Project
 
-    constructor(textField: JTextField) {
+    constructor(textField: JTextField, project: Project) {
         this.textField = textField
+        this.project = project
     }
 
     override fun actionPerformed(e: ActionEvent) {
-        val uri = textField.getText()
+        val uri = textField.text
         log.info("clicked!, uri: $uri")
+
+        val connectedNotifier = project.messageBus.syncPublisher(ConnectedNotifier.CONNECTED_TOPIC)
+        connectedNotifier.afterAction()
     }
 }
--- src/main/kotlin/org/koiki/beepro/dancer/intellij/ui/MainToolWindowFactory.kt
+++ src/main/kotlin/org/koiki/beepro/dancer/intellij/ui/MainToolWindowFactory.kt
@@ -4,10 +4,12 @@ import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import org.koiki.beepro.dancer.intellij.websocket.ConnectedNotifier
 import java.awt.Color
 import java.awt.Font
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
+import javax.naming.Context
 import javax.swing.JButton
 import javax.swing.JLabel
 import javax.swing.JPanel
@@ -19,13 +21,23 @@ class MainToolWindowFactory : ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         log.info("factory starts")
 
+
+        val bus = project.messageBus
+        bus.connect().subscribe(ConnectedNotifier.CONNECTED_TOPIC, object : ConnectedNotifier {
+            private val log = Logger.getInstance(this::class.java)
+            override fun afterAction() {
+                log.info("I got notification by clicking button!")
+            }
+        })
+
+
         val label = JLabel("Hello World!")
 
         val textField = JTextField(8)
         textField.setFont(Font("ＭＳ ゴシック", Font.BOLD, 16))
 
         val button = JButton()
-        button.addActionListener(ConnectButtonListene(textField))
+        button.addActionListener(ConnectButtonListener(textField, project))
 
         val jPanel = JPanel()
         jPanel.add(label)
